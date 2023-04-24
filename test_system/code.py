3bf9d34535887731744136c1320b39064281e580
aydar <aydar.baysuvakov@gmail.com>
2023-04-24 19:00:00 +0500

    Adding to Glitch

--- test_system/code.py
+++ test_system/code.py
@@ -15,7 +15,20 @@ def main():
         f_write.write(text)
 
 
-    print(sum(map(int, input().split())))
+    try:
+        f, s = input().split('-')
+        f1 = 'ABCDEFGH'.index(f[0])
+        s1 = 'ABCDEFGH'.index(s[0])
+        f2 = int(f[1])
+        s2 = int(s[1])
+        if abs(s1 - f1) == 2 and abs(s2 - f2) == 1:
+            print('YES')
+        elif abs(s1 - f1) == 1 and abs(s2 - f2) == 2:
+            print('YES')
+        else:
+            print('NO')
+    except:
+        print('ERROR')
 
     f_read.close()
     f_write.close()
